b0180e31e1372a95fb9a9df282afeff45cd4c453
cuong <ndcuong912@gmail.com>
2023-03-19 19:55:53 +0700

    update structure

--- product/src/App.js
+++ product/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import api from './api/axiosConfig'
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState,useEffect } from 'react';
+import Layout from './components/Layout';
+import HeroDetail from './components/hero/HeroDetail';
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import "./fontawsome";
 function App() {
+  const [flag,setFlag] = useState([]);
+  const [singleFlag,setSingleFlag] = useState([]);
+  const [border,setBorder] = useState([]);
+  const getFlags = async ()=> {
+    try{
+      const res = await api.get("/v3.1/all");
+      
+      setFlag(res.data);
+      console.log(res.data);
+     
+    }catch(err){
+      console.log(err);
+    }
+   
+  }
+  const getCountryDetail = async(name)=>{
+    setBorder([]);
+    try{
+      const res = await api.get(`/v3.1/name/${name}?fullText=true`);
+      setSingleFlag(res.data);
+      res.data[0].borders.map(async bcode=>{
+        try{
+          const border = await  api.get(`https://restcountries.com/v3.1/alpha/${bcode}`);
+          setBorder(borders=>[...borders,border.data]);
+        }catch(err){
+          console.log(err);
+        }
+        
+      })
+      
+    }catch(err){
+        console.log(err);
+    }
+  }
+  useEffect(()=>{
+    getFlags();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+                <h1>Where in the world ?</h1>
+                <div className="theme">
+                    <FontAwesomeIcon icon=" fa-moon" />
+                    <p className="theme__type">
+                        Dark mode
+                    </p>
+                </div>
+        </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home flags={flag}/>}></Route>
+          <Route path='/detail/:name' element={<HeroDetail border={border} getCountryDetail={getCountryDetail} singleFlag={singleFlag} />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
